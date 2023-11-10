e40f2931a272f1252cf79cb63b0daa15eea064c6
Noureddin <noureddin95@gmail.com>
2023-11-10 14:56:01 +0300

    fix error on changing lessons, b/c minification

--- .p/minifier.pl
+++ .p/minifier.pl
@@ -5,14 +5,17 @@ use open qw[ :encoding(UTF-8) :std ];
 die "must provide an argument: js, css, or html\n"
   unless @ARGV;
 
-exec qw[ deno run --quiet --allow-read npm:uglify-js --compress top_retain='[play]',passes=5 --mangle toplevel,reserved='[play]' ], @ARGV[1..$#ARGV]
+my @files = @ARGV[1..$#ARGV];
+
+# NOTE: exactly three passes give the smallest size; more would inline play() but keep its definition so it'd give larger results
+exec qw[ deno run --quiet --allow-read npm:uglify-js --compress top_retain=[play],passes=3 --mangle toplevel,reserved=[play] ], @files
   if $ARGV[0] eq 'js';
 
-exec qw[ deno run --quiet --allow-read npm:clean-css-cli ], @ARGV[1..$#ARGV]
+exec qw[ deno run --quiet --allow-read npm:clean-css-cli ], @files
   if $ARGV[0] eq 'css';
 
-exec qw[ perl -CSAD .p/html-minify.pl ], @ARGV[1..$#ARGV]
+exec qw[ perl -CSAD .p/html-minify.pl ], @files
   if $ARGV[0] eq 'html';
 
-die "Unexpect argument: '$ARGV[0]'; expected: js, css, or html\n"
+die "Unexpect argument: '$ARGV[0]'; expected: js, css, or html\n";
 
